b88be5135de0657d37f39238bf5928b1799cc47f
Sonjieun <sonjieun2661@gmail.com>
2024-05-22 21:40:07 +0900

    0522

--- example/src/App.js
+++ example/src/App.js
@@ -2,28 +2,21 @@ import React, { useState } from "react";
 import Form from "./Form";
 
 function App() {
-  const [year, setYear] = useState(1990);
-  const [stringRes, setStringRes] = useState("성인?미성년자?");
-  const [age, setAge] = useState(0);
+  const [mode, setMode] = useState(false);
 
   return (
     <div className="App">
-      <Form
-        value={year}
-        onChange={(e) => {
-          setYear(e.target.value);
-        }}
-        onClick={() => {
-          setAge(2024 - year);
-          if (2024 - year > 19) {
-            setStringRes("성인");
-          } else {
-            setStringRes("미성년자");
-          }
-        }}
-      ></Form>
-      <h1>{stringRes}</h1>
-      <h1>{age}</h1>
+      {mode ? (
+        <Form setMode={setMode} />
+      ) : (
+        <button
+          onClick={() => {
+            setMode(true);
+          }}
+        >
+          Start!
+        </button>
+      )}
     </div>
   );
 }
--- example/src/Form.jsx
+++ example/src/Form.jsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = (props) => {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
   return (
     <div>
-      <input
-        type="number"
-        value={props.value}
-        min={1990}
-        max={2024}
-        onChange={props.onChange}
-      />
-      <button onClick={props.onClick}>ok</button>
+      <h1>사용자 정보 입력</h1>
+      <div>
+        <label for="text">이름</label>
+        <input
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          placeholder="name"
+        />
+      </div>
+      <div>
+        <label for="date">생년월일</label>
+        <input
+          value={age}
+          onChange={(e) => {
+            setAge(e.target.value);
+          }}
+          type="date"
+          placeholder="age"
+        />
+      </div>
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          window.alert(`welcome ${name}!`);
+          props.setMode(false);
+        }}
+      >
+        ok
+      </button>
     </div>
   );
 };
